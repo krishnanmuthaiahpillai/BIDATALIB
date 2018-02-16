82778f840f7578c2d016bc80c7aa300e302030e5
krishnanmuthaiahpillai <krishnanmuthaiahpillai@gmail.com>
2018-02-16 18:35:11 +0530

    video added

---
+++ library/src/main/java/com/bidata/mylibrary/VideoCompressListener.java
@@ -0,0 +1,11 @@
+package com.bidata.mylibrary;
+
+import java.io.File;
+
+/**
+ * Created by Dell-OP9010 on 11/29/2017.
+ */
+
+public interface VideoCompressListener {
+    public void Oncomplete(File file);
+}
---
+++ library/src/main/java/com/bidata/mylibrary/VideoCompressor.java
@@ -0,0 +1,47 @@
+package com.bidata.mylibrary;
+
+import android.content.Context;
+import android.os.AsyncTask;
+import com.iceteck.silicompressorr.SiliCompressor;
+import java.io.File;
+
+/**
+ * Created by Dell-OP9010 on 11/29/2017.
+ */
+
+public class VideoCompressor extends AsyncTask<String, Void, String> {
+    Context context;
+    File infile;
+    File savedir;
+    VideoCompressListener listener;
+
+    public VideoCompressor(Context context, File infile, File savedir, VideoCompressListener listener){
+        this.context=context;
+        this.infile=infile;
+        this.savedir=savedir;
+        this.listener=listener;
+
+    }
+
+    @Override
+    protected String doInBackground(String... params) {
+        String filePath=infile.getAbsolutePath();
+        try {
+            filePath = SiliCompressor.with(context).compressVideo(infile.getAbsolutePath(), savedir.getAbsolutePath());
+            if(new File(filePath).exists()){
+                infile.delete();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return filePath;
+    }
+
+    @Override
+    protected void onPostExecute(String filepath) {
+        listener.Oncomplete(new File(filepath));
+    }
+}
+
+
